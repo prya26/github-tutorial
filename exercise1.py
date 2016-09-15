59bcc20c44adc7dd7ff36c9ddb61d73edd056079
prya26 <prya26@tamu.edu>
2016-09-15 13:07:28 -0700

    Added art

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,17 @@
 def print_message():
 	print "Git git git!"
 
+def print_art():
+	print(("""\
+
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \ 
+                                  ,"  ## |   ಠ ಠ. 
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+
+
+                    """).encode('utf-8'))
